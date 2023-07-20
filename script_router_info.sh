7272d81cfa74c90db2cac12f226bf5c14fa80e06
Noorcyber <100744631+Noorcyber@users.noreply.github.com>
2023-07-20 09:29:09 -0400

    Update script_router_info.sh

--- script_router_info.sh
+++ script_router_info.sh
@@ -1,27 +1,37 @@
 #!/bin/bash
 
+# Function to echo and then "run" a command
+run_cmd() {
+    local command="$@"
+    echo "Running command: $command"
+}
+
 ping_host() {
-    echo "Ping not supported in this environment."
+    run_cmd "ping -c 4 $1"
 }
 
 traceroute_host() {
-    echo "Traceroute not supported in this environment."
+    run_cmd "traceroute $1"
 }
 
 get_network_interfaces() {
-    echo "Network interfaces not available in this environment."
+    run_cmd "ifconfig"
 }
 
 get_arp_table() {
-    echo "ARP table not available in this environment."
+    run_cmd "arp -a"
 }
 
 iperf_speed_test() {
-    echo "IPERF speed test not supported in this environment."
+    run_cmd "iperf3 -R -c $1 -P 16"
 }
 
 ping_virtual_clients() {
-    echo "Ping virtual clients not supported in this environment."
+    local router_ip="$1"
+    shift
+    for client_ip in "$@"; do
+        run_cmd "ping -c 4 $client_ip"
+    done
 }
 
 # Example usage
@@ -29,27 +39,31 @@ echo "Enter an IP address:"
 read -r target_ip
 
 # Ping test
-ping_output=$(ping_host "$target_ip")
-echo "Ping Test:"
-echo "$ping_output"
+ping_host "$target_ip"
+echo "Ping Test: (No output in this environment)"
 
 # Traceroute test
-traceroute_output=$(traceroute_host "$target_ip")
-echo "Traceroute:"
-echo "$traceroute_output"
+traceroute_host "$target_ip"
+echo "Traceroute: (No output in this environment)"
 
 # Network Interfaces
-network_interfaces_output=$(get_network_interfaces)
-echo "Network Interfaces:"
-echo "$network_interfaces_output"
+get_network_interfaces
+echo "Network Interfaces: (No output in this environment)"
 
 # ARP Table
-arp_table_output=$(get_arp_table)
-echo "ARP Table:"
-echo "$arp_table_output"
+get_arp_table
+echo "ARP Table: (No output in this environment)"
 
 # IPERF Speed Test
-echo "IPERF speed test not supported in this environment."
+echo "Enter the IP address of the iperf server:"
+read -r iperf_server_ip
+iperf_speed_test "$iperf_server_ip"
+echo "IPERF Speed Test: (No output in this environment)"
 
 # Ping Virtual Clients
-echo "Ping virtual clients not supported in this environment."
+echo "Enter the IP address of the router:"
+read -r router_ip
+echo "Enter a comma-separated list of virtual client IP addresses:"
+read -r -a virtual_clients
+ping_virtual_clients "$router_ip" "${virtual_clients[@]}"
+echo "Ping Virtual Clients: (No output in this environment)"
