d74767f4d30c3fa8108fa4d4d45a9fa73a58e8a6
Noorcyber <100744631+Noorcyber@users.noreply.github.com>
2025-02-11 16:34:37 -0500

    Update script_router_info.sh

--- script_router_info.sh
+++ script_router_info.sh
@@ -4,6 +4,7 @@
 run_cmd() {
     command="$@"
     echo "Running command: $command"
+    $command
 }
 
 ping_host() {
@@ -15,13 +16,11 @@ traceroute_host() {
 }
 
 get_network_interfaces() {
-    run_cmd "ifconfig"
+    run_cmd "ip addr show"
 }
 
 get_arp_table() {
-    echo "IP address       HW type     Flags       HW address            Mask     Device"
-    echo "192.168.1.1      0x1         0x2         00:11:22:33:44:55     *        eth0"
-    echo "192.168.1.100    0x1         0x2         12:34:56:78:90:ab     *        wlan0"
+    run_cmd "arp"
 }
 
 iperf_speed_test() {
@@ -42,25 +41,20 @@ read -r target_ip
 
 # Ping test
 ping_host "$target_ip"
-echo "Ping Test: (No output in this environment)"
 
 # Traceroute test
 traceroute_host "$target_ip"
-echo "Traceroute: (No output in this environment)"
 
 # Network Interfaces
 get_network_interfaces
-echo "Network Interfaces: (No output in this environment)"
 
 # ARP Table
 get_arp_table
-echo "ARP Table: (No output in this environment)"
 
 # IPERF Speed Test
 echo "Enter the IP address of the iperf server:"
 read -r iperf_server_ip
 iperf_speed_test "$iperf_server_ip"
-echo "IPERF Speed Test: (No output in this environment)"
 
 # Ping Virtual Clients
 echo "Enter the IP address of the router:"
@@ -68,4 +62,3 @@ read -r router_ip
 echo "Enter a comma-separated list of virtual client IP addresses:"
 read -r -a virtual_clients
 ping_virtual_clients "$router_ip" "${virtual_clients[@]}"
-echo "Ping Virtual Clients: (No output in this environment)"
